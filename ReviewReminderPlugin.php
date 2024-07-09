b5867872a5a27be9054b9398d0ff127be5aa1608
yves <yves@lepidus.com.br>
2024-07-09 16:11:46 -0400

    Initial project.
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#850
    
    Signed-off-by: yves <yves@lepidus.com.br>

---
+++ ReviewReminderPlugin.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * @file plugins/generic/reviewReminder/ReviewReminderPlugin.inc.php
+ *
+ * Copyright (c) 2024 Lepidus Tecnologia
+ * Distributed under the GNU GPL v3. For full terms see the file LICENSE.
+ *
+ * @class ReviewReminderPlugin
+ * @ingroup plugins_generic_reviewReminder
+ *
+ * @brief Plugin for sending review reminder emails to designated reviewers
+ */
+
+namespace APP\plugins\generic\reviewReminder;
+
+use PKP\plugins\GenericPlugin;
+
+class ReviewReminderPlugin extends GenericPlugin
+{
+    public function register($category, $path, $mainContextId = null)
+    {
+        $success = parent::register($category, $path, $mainContextId);
+
+        return $success;
+    }
+
+    public function getDisplayName()
+    {
+        return __('plugins.generic.reviewReminder.displayName');
+    }
+
+    public function getDescription()
+    {
+        return __('plugins.generic.reviewReminder.description');
+    }
+
+}
