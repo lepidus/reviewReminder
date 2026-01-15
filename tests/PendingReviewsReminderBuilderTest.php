b0cb3e7004e4e81103df8307204c6eca0a48af68
Jhon <jhon@lepidus.com.br>
2026-01-15 14:22:57 -0400

    WIP: Creation of test for pending reviews reminder email builder
    Issue: documentacao-e-tarefas/scielo#876
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

---
+++ tests/PendingReviewsReminderBuilderTest.php
@@ -0,0 +1,89 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use APP\core\Application;
+use APP\journal\Journal;
+use PKP\user\User;
+use APP\submission\Submission;
+use APP\publication\Publication;
+use APP\plugins\generic\reviewReminder\classes\PendingReviewsReminderBuilder;
+
+class PendingReviewsReminderBuilderTest extends TestCase
+{
+    private $locale = 'en';
+    private $context;
+    private $reviewer;
+    private $submissions;
+    private $pendingReviewsReminderBuilder;
+
+    public function setUp(): void
+    {
+        $this->context = $this->createTestContext();
+        $this->reviewer = $this->createReviewerUser();
+        $this->submissions = $this->createTestSubmissions();
+        $this->pendingReviewsReminderBuilder = new PendingReviewsReminderBuilder(
+            $this->context,
+            $this->reviewer,
+            $this->submissions,
+            $this->locale
+        );
+    }
+
+    private function createTestContext()
+    {
+        $context = new Journal();
+        $context->setData('name', 'Example Journal', $this->locale);
+        $context->setData('contactName', 'Example contact');
+        $context->setData('contactEmail', 'example.contact@gmail.com');
+
+        return $context;
+    }
+
+    private function createReviewerUser(): User
+    {
+        $reviewer = new User();
+        $reviewer->setData('id', 2024);
+        $reviewer->setData('email', 'juancarlo.rodriguez@gmail.com');
+        $reviewer->setData('givenName', 'Juan Carlo', $this->locale);
+        $reviewer->setData('familyName', 'Rodriguez', $this->locale);
+
+        return $reviewer;
+    }
+
+    private function createTestSubmissions(): array
+    {
+        $fiveDaysLater = new DateTime();
+        $sixDaysLater = new DateTime();
+        $fiveDaysLater = $fiveDaysLater->modify('+5 days')->format('Y-m-d H:i:s');
+        $sixDaysLater = $sixDaysLater->modify('+6 days')->format('Y-m-d H:i:s');
+
+        $firstPublication = new Publication();
+        $firstPublication->setAllData([
+            'id' => 147,
+            'title' => [$this->locale => 'First Submission Title'],
+        ]);
+        $firstSubmission = new Submission();
+        $firstSubmission->setAllData([
+            'id' => 123,
+            'currentPublicationId' => $firstPublication->getId(),
+            'publications' => [$firstPublication],
+        ]);
+
+        $secondPublication = new Publication();
+        $secondPublication->setAllData([
+            'id' => 148,
+            'title' => [$this->locale => 'Second Submission Title'],
+        ]);
+        $secondSubmission = new Submission();
+        $secondSubmission->setAllData([
+            'id' => 124,
+            'currentPublicationId' => $secondPublication->getId(),
+            'publications' => [$secondPublication],
+        ]);
+
+        return [
+            ['submission' => $firstSubmission, 'reviewDueDate' => $fiveDaysLater],
+            ['submission' => $secondSubmission, 'reviewDueDate' => $sixDaysLater],
+        ];
+    }
+}
